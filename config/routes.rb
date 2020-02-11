b8cf36f8004bd18e1bd711530594172fc8104eaa
carlosMoreno357 <carlos.moreno.salazar@gmail.com>
2020-02-11 16:01:20 -0600

    tryin root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html	
-  resources :users, only: [:new, :create]
+  resources :leads, path:'', :users, only: [:new, :create]
   root 'sessions#welcome'
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#logout'
