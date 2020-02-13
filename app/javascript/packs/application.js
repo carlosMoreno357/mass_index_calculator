4536b0778f1109dd6134c1bc5aa70b55f4d14065
carlosMoreno357 <carlos.moreno.salazar@gmail.com>
2020-02-13 12:42:46 -0600

    all forms now validate input

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("semantic-ui")
+//require("semantic-ui")
 //Main javascript features for mass index calculations
 require("packs/massIndex")
 
--- app/javascript/packs/massIndex.js
+++ app/javascript/packs/massIndex.js
@@ -6,10 +6,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
   const resultLabel = document.querySelector("#body_mass_index");
 
   const massCalculation = () => {
-    bodyMass.value === '' || height === ''
-    ? alert("You must set a value for both fields")
-    : resultLabel.innerHTML = (bodyMass.value/Math.pow(height.value,2)).toFixed(2) + ' kg/m²'
+    if(bodyMass.value === '' || height === ''){
+      resultLabel.innerHTML = 'You must set a value for both fields.';
+    }else{
+      let result = (bodyMass.value/Math.pow(height.value,2)).toFixed(2);
+      if(result <= 0.009){
+        resultLabel.innerHTML = "The result is lower than 0.00 kg/m², you should try with other values.";
+        bodyMass.value = "";
+        height.value= "";
+      }else{
+        resultLabel.innerHTML = result + ' kg/m²';
+      }
+    }
+    resultLabel.classList.remove('hidden');
+    resultLabel.classList.add('visible');
   }
-  
-  calculateMass.addEventListener("click", massCalculation, false);
+  calculateMass.addEventListener('click', massCalculation, false);
 });
